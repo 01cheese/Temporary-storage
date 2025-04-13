8027a32b286ec198026c5bb375e74ca90bba61bb
Vadym Zelenko <115219323+01cheese@users.noreply.github.com>
2025-04-13 11:26:40 +0200

    Update file.routes.js

--- server/routes/file.routes.js
+++ server/routes/file.routes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import multer from "multer";
 import { uploadFile, getFile, checkFile } from "../controllers/file.controller.js";
+import { downloadAllAsZip } from "../controllers/file.controller.js";
+
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/upload", upload.array("files"), uploadFile);
 router.get("/:id", getFile);
-router.get("/check/:id", checkFile); // ✅
+router.get("/check/:id", checkFile);
+router.get("/:id/zip", downloadAllAsZip);
 
 export default router;
