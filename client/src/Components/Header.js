3ec6f77e18cd741982a9e3249363878b290529c0
Vadym Zelenko <115219323+01cheese@users.noreply.github.com>
2025-04-18 00:19:01 +0200

    Update Header.js

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -9,9 +9,9 @@ function Header() {
             <input type="checkbox" id="menu-toggle" className="menu-toggle" />
             <label htmlFor="menu-toggle" className="burger">&#9776;</label>
             <nav className="nav">
-                <a href="https://drive.vzbb.site/">Main</a>
-                <a href="#">About Project</a>
-                <a href="#">GitHub</a>
+                <a href="https://temporary-storage-f.onrender.com/">Main</a>
+                <a href="https://github.com/01cheese/Temporary-storage">About Project</a>
+                <a href="https://github.com/01cheese/Temporary-storage">GitHub</a>
             </nav>
         </header>
     );
